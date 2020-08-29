a9785aa692d94319a8d37ec24de978b9f5950977
Roman Yakovlev <tensegrity666@icloud.com>
2020-08-30 01:25:15 +0300

    init: start project

---
+++ src/components/app/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './index.module.css';
+
+const App = () => {
+  const { app } = styles;
+
+  return (
+    <div className={app}>
+    </div>
+  );
+}
+
+export default App;
