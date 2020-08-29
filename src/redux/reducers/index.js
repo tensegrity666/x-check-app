b55a6edf607ef3dee0bdd2d27f33139905aa2459
Roman Yakovlev <tensegrity666@icloud.com>
2020-08-30 02:35:04 +0300

    feat: add example

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,6 @@
+// import actionTypes from '../constants';
+// import initialState from '../initial-state';
+
+// const reducer = (state = initialState, { type, payload }) => {}
+
+// export default reducer;
