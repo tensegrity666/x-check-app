eb81ea8f27dfe4e4a33dfcf23036469799320df3
Roman Yakovlev <tensegrity666@icloud.com>
2020-09-02 21:43:58 +0300

    feat: setup environment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/app';
+import App from './app';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
