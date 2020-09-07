e44415d8980f6f691be0c07863d3ae3dc401a575
DEnFUrt <musorov@gmail.com>
2020-09-07 11:41:46 +0500

    feat: add base method, method for user, task, items task (#3)

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ShowRestApi from '../components/show-rest-api';
 
 const Auth = () => {
   return (
     <>
       <h1>Authorization</h1>
-      <ShowRestApi />
     </>
   );
 };
