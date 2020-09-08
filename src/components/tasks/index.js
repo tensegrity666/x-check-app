e608b783119bfecf172d11c2e0cd39ea797f72cb
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-09-08 17:43:37 +0600

    feat: add displaying of task creation date in tasks list

--- src/components/tasks/index.js
+++ src/components/tasks/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { Typography, List } from 'antd';
-import { Link as RouterLink } from 'react-router-dom';
+import { ProfileOutlined } from '@ant-design/icons';
 
 import tasksList from './mockTasksList.json';
 import styles from './index.module.css';
@@ -8,22 +9,38 @@ import styles from './index.module.css';
 const { Title } = Typography;
 
 const Tasks = () => {
+  const history = useHistory();
+
+  const handleProceedToTask = (taskId) => {
+    history.push(`/tasks${taskId}`);
+  };
+
   return (
     <div>
       <Typography>
-        <Title>Tasks</Title>
+        <Title>Список задач</Title>
       </Typography>
       <section>
         <List
           dataSource={tasksList}
-          renderItem={({ id, taskTitle, state }) => (
+          renderItem={({ id, taskTitle, dateOfCreate, state }) => (
             <List.Item>
-              <RouterLink to={`/tasks/${id}`} className={styles.tasksLink}>
-                {state !== 'PUBLISHED' && (
-                  <Typography.Text mark>{`[${state}] `}</Typography.Text>
-                )}
-                {taskTitle}
-              </RouterLink>
+              <List.Item.Meta
+                className={styles.tasksMeta}
+                onClick={() => handleProceedToTask(id)}
+                avatar={<ProfileOutlined style={{ fontSize: '30px' }} />}
+                title={
+                  state !== 'PUBLISHED' ? (
+                    <>
+                      <Typography.Text mark>{`[${state}] `}</Typography.Text>
+                      {taskTitle}
+                    </>
+                  ) : (
+                    taskTitle
+                  )
+                }
+                description={`дата создания: ${dateOfCreate}`}
+              />
             </List.Item>
           )}
         />
