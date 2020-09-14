ebd9ce86048a418bd60598da10e694772ba376e2
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-09-14 21:14:00 +0600

    feat: update session assignment for using redux store

--- src/pages/sessions.js
+++ src/pages/sessions.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import SessionAssignmentDialog from '../components/session-assignment-dialog';
 
-const Session = () => {
+const Sessions = () => {
   const { id } = useParams();
   return (
     <>
       <h1>Crosscheck session#{id}</h1>
+      <SessionAssignmentDialog />
     </>
   );
 };
 
-export default Session;
+export default Sessions;
