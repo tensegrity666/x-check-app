47fade381c189a688a4e7585173171bcded34208
Andrey Lavrov <fanton96mailru@gmail.com>
2020-09-16 02:03:15 +0300

    refactor: transform request-table to functional component

--- src/components/requests-table/index.js
+++ src/components/requests-table/index.js
@@ -1,127 +1,54 @@
-import React, { Component } from 'react';
-import { Table, Input, Button, Space } from 'antd';
-import Highlighter from 'react-highlight-words';
-import { SearchOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import { Table } from 'antd';
 import { tableColumns, pagination } from './constants';
 import defaultCustomers from './mockData';
+import getColumnSearchProps from './utils';
 import styles from './index.module.css';
 
-export default class RequestsTable extends Component {
-  constructor() {
-    super();
-    this.state = {
-      searchText: '',
-      searchedColumn: '',
-    };
-  }
+const RequestsTable = () => {
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
 
-  getColumnSearchProps = (dataIndex) => {
-    const { searchedColumn, searchText } = this.state;
-    return {
-      filterDropdown: ({
-        setSelectedKeys,
-        selectedKeys,
-        confirm,
-        clearFilters,
-      }) => (
-        <div style={{ padding: 8 }}>
-          <Input
-            ref={(node) => {
-              this.searchInput = node;
-            }}
-            placeholder={`Search ${dataIndex}`}
-            value={selectedKeys[0]}
-            onChange={(e) =>
-              setSelectedKeys(e.target.value ? [e.target.value] : [])
-            }
-            onPressEnter={() =>
-              this.handleSearch(selectedKeys, confirm, dataIndex)
-            }
-            style={{ width: 188, marginBottom: 8, display: 'block' }}
-          />
-          <Space>
-            <Button
-              type="primary"
-              onClick={() =>
-                this.handleSearch(selectedKeys, confirm, dataIndex)
-              }
-              icon={<SearchOutlined />}
-              size="small"
-              style={{ width: 90 }}>
-              Search
-            </Button>
-            <Button
-              onClick={() => this.handleReset(clearFilters)}
-              size="small"
-              style={{ width: 90 }}>
-              Reset
-            </Button>
-          </Space>
-        </div>
-      ),
-      filterIcon: (filtered) => (
-        <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
-      ),
-      onFilter: (value, record) =>
-        record[dataIndex]
-          ? record[dataIndex]
-              .toString()
-              .toLowerCase()
-              .includes(value.toLowerCase())
-          : '',
-      onFilterDropdownVisibleChange: (visible) => {
-        if (visible) {
-          setTimeout(() => this.searchInput.select(), 100);
-        }
-      },
-      render: (text) =>
-        searchedColumn === dataIndex ? (
-          <Highlighter
-            highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
-            searchWords={[searchText]}
-            autoEscape
-            textToHighlight={text ? text.toString() : ''}
-          />
-        ) : (
-          text
-        ),
-    };
-  };
-
-  handleSearch = (selectedKeys, confirm, dataIndex) => {
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
     confirm();
-    this.setState({
-      searchText: selectedKeys[0],
-      searchedColumn: dataIndex,
-    });
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
   };
 
-  handleReset = (clearFilters) => {
+  const handleReset = (clearFilters) => {
     clearFilters();
-    this.setState({ searchText: '' });
+    setSearchText('');
   };
 
-  render() {
-    const columnsWithSearch = tableColumns.map((el) => {
-      const searchOptions = el.isSearchable
-        ? { ...this.getColumnSearchProps(el.dataIndex) }
-        : null;
-      return {
-        ...el,
-        ...searchOptions,
-      };
-    });
+  const columnsWithSearch = tableColumns.map((el) => {
+    const searchOptions = el.isSearchable
+      ? {
+          ...getColumnSearchProps(
+            el.dataIndex,
+            handleSearch,
+            handleReset,
+            searchText,
+            searchedColumn
+          ),
+        }
+      : null;
+    return {
+      ...el,
+      ...searchOptions,
+    };
+  });
 
-    return (
-      <div className={styles.reviewRequests}>
-        <h1>Review Requests</h1>
-        <Table
-          dataSource={defaultCustomers}
-          columns={columnsWithSearch}
-          pagination={pagination}
-          scroll={{ x: 1200, y: 'calc(100vh - 210px)' }}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={styles.reviewRequests}>
+      <h1>Review Requests</h1>
+      <Table
+        dataSource={defaultCustomers}
+        columns={columnsWithSearch}
+        pagination={pagination}
+        scroll={{ x: 1200, y: 'calc(100vh - 210px)' }}
+      />
+    </div>
+  );
+};
+
+export default RequestsTable;
