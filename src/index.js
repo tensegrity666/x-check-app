9cbc45c045ec312bc842e4b7bc613d1ae67040f2
DEnFUrt <musorov@gmail.com>
2020-09-19 14:22:46 +0500

    feat: add check for one-time use task

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
-import App from './app';
+// import App from './app';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/store';
 import './index.css';
+import ShowRestApi from './show-rest-api';
 
 render(
   <Provider store={store}>
-    <App />
+    <ShowRestApi>{/* <App /> */}</ShowRestApi>
   </Provider>,
   document.getElementById('root')
 );
