3ffab8e7c56f794a1a048c0bc4541735bec4e45a
Roman Yakovlev <tensegrity666@icloud.com>
2020-09-20 23:28:44 +0300

    feat: add header

---
+++ src/components/header/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Button, Avatar } from 'antd';
+
+import logo from './assets/logo-rsschool3.png';
+import styles from './index.module.css';
+
+const Header = () => {
+  const { nav, avatar } = styles;
+  const { photoURL } = useSelector(({ loginReducer }) => loginReducer);
+
+  return (
+    <nav className={nav}>
+      <a
+        href="https://app.rs.school/"
+        target="_blank"
+        rel="noopener noreferrer">
+        <img src={logo} alt="Rolling scopes school logo" height="30" />
+      </a>
+      <Button
+        size="large"
+        icon={<Avatar className={avatar} src={photoURL} size={24} />}
+        type="dashed">
+        My Profile
+      </Button>
+    </nav>
+  );
+};
+
+export default Header;
