b9dbdd0846f4e8c8195fdeafd8edb719afeb7ea8
DEnFUrt <musorov@gmail.com>
2020-09-22 01:39:40 +0500

    refactor: before rebase

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
-// import App from './app';
+import App from './app';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/store';
 import './index.css';
-import ShowRestApi from './show-rest-api';
 
 render(
   <Provider store={store}>
-    <ShowRestApi>{/* <App /> */}</ShowRestApi>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
