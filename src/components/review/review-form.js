c550d5fb80f862b84defd2eef88b05e903594132
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-09-24 13:23:16 +0600

    feat: add table helper to populate row with keys

--- src/components/review/review-form.js
+++ src/components/review/review-form.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Table } from 'antd';
 
 import { REVIEW_REQUEST } from '../../types';
-
-import mockSelfGrade from './mockSelfGrade.json';
+import populateRowsWithKeys from './table-helpers';
 
 const ReviewForm = ({ reviewRequest }) => {
   const { Column } = Table;
+
   return (
     <Table
-      dataSource={reviewRequest.selfGrade || mockSelfGrade}
+      dataSource={populateRowsWithKeys(reviewRequest.selfGrade)}
       pagination={false}>
       <Column title="Comment" dataIndex="comment" />
       <Column title="Score" dataIndex="score" />
