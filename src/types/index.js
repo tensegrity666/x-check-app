87ba30fb9d54582bba85bfeb109634a090d3cf5a
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-09-24 13:19:46 +0600

    chore: update prop types with changed slef grade structure

--- src/types/index.js
+++ src/types/index.js
@@ -1,11 +1,17 @@
 /* eslint-disable import/prefer-default-export */
 import PropTypes from 'prop-types';
 
+export const SELF_GRADE_ITEM = PropTypes.shape({
+  id: PropTypes.string,
+  score: PropTypes.string,
+  comment: PropTypes.string,
+});
+
 export const REVIEW_REQUEST = PropTypes.shape({
   id: PropTypes.string,
   crossCheckSessionId: PropTypes.string,
   author: PropTypes.string,
   task: PropTypes.string,
   state: PropTypes.string,
-  selfGrade: PropTypes.object,
+  selfGrade: PropTypes.arrayOf(SELF_GRADE_ITEM),
 });
