82e20a5ac11b67ba414801ee04ade290d9097e64
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-09-25 21:21:25 +0600

    feat: add conditional controls for triggering review effects

--- src/components/review/authors-controls.js
+++ src/components/review/authors-controls.js
@@ -1,15 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'antd';
+import { Space, Button } from 'antd';
 
 import { REVIEW_STATE } from './constants';
+import { actionReviewList as modify } from '../../services/rest-api/constants';
 
-const AuthorsControls = ({ reviewStatus }) => {
-  return reviewStatus === REVIEW_STATE.DRAFT && <Button>Publish</Button>;
+const AuthorsControls = ({
+  reviewStatus,
+  createReview,
+  editReview,
+  toggleReviewStatus,
+}) => {
+  const handlePublish = async () => {
+    await toggleReviewStatus(modify.DRAFT_TO_PUBLISHED);
+    await editReview();
+  };
+
+  if (reviewStatus === REVIEW_STATE.DRAFT) {
+    return (
+      <Space>
+        <Button onClick={createReview}>Save Draft</Button>
+        <Button onClick={handlePublish}>Publish</Button>
+      </Space>
+    );
+  }
+
+  if (reviewStatus === REVIEW_STATE.PUBLISHED) {
+    return (
+      <Space>
+        <Button onClick={editReview}>Save</Button>
+      </Space>
+    );
+  }
+
+  if (reviewStatus === REVIEW_STATE.DISPUTED) {
+    return (
+      <Space>
+        <Button onClick={editReview}>Save</Button>
+        <Button onClick={() => toggleReviewStatus(modify.DISPUTED_TO_ACCEPTED)}>
+          Accept
+        </Button>
+      </Space>
+    );
+  }
+
+  if (reviewStatus === REVIEW_STATE.ACCEPTED) return null;
+  return null;
 };
 
 AuthorsControls.propTypes = {
   reviewStatus: PropTypes.string.isRequired,
+  createReview: PropTypes.func.isRequired,
+  editReview: PropTypes.func.isRequired,
+  toggleReviewStatus: PropTypes.func.isRequired,
 };
 
 export default AuthorsControls;
--- src/components/review/students-controls.js
+++ src/components/review/students-controls.js
@@ -1,25 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'antd';
+import { Space, Button } from 'antd';
 
 import { REVIEW_STATE } from './constants';
+import { actionReviewList as modify } from '../../services/rest-api/constants';
 
-const StudentsControls = ({ reviewStatus, onDisputeReview }) => {
-  return (
-    reviewStatus === REVIEW_STATE.PUBLISHED && (
-      <>
-        <Button danger onClick={onDisputeReview}>
+const StudentsControls = ({
+  reviewStatus,
+  onDisputeReview,
+  toggleReviewStatus,
+  editReview,
+}) => {
+  const handleReviewDispute = async () => {
+    onDisputeReview();
+    await toggleReviewStatus(modify.PUBLISHED_TO_DISPUTED);
+  };
+
+  if (
+    reviewStatus === REVIEW_STATE.DRAFT ||
+    reviewStatus === REVIEW_STATE.ACCEPTED
+  )
+    return null;
+
+  if (reviewStatus === REVIEW_STATE.PUBLISHED) {
+    return (
+      <Space>
+        <Button danger onClick={handleReviewDispute}>
           Dispute
         </Button>
-        <Button>Accept</Button>
-      </>
-    )
-  );
+        <Button
+          onClick={() => toggleReviewStatus(modify.PUBLISHED_TO_DISPUTED)}>
+          Accept
+        </Button>
+      </Space>
+    );
+  }
+
+  if (reviewStatus === REVIEW_STATE.DISPUTED) {
+    return (
+      <Space>
+        <Button onClick={editReview}>Save</Button>
+        <Button onCLick={() => toggleReviewStatus(modify.DISPUTED_TO_ACCEPTED)}>
+          Accept
+        </Button>
+      </Space>
+    );
+  }
+
+  return null;
 };
 
 StudentsControls.propTypes = {
   reviewStatus: PropTypes.string.isRequired,
   onDisputeReview: PropTypes.func.isRequired,
+  toggleReviewStatus: PropTypes.func.isRequired,
+  editReview: PropTypes.func.isRequired,
 };
 
 export default StudentsControls;
