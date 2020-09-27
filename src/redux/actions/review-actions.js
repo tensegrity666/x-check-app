b92e160affa0adb540136e2af0674cd0c3f2c065
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-09-27 15:46:07 +0600

    feat: add refreshing review state after updating

--- src/redux/actions/review-actions.js
+++ src/redux/actions/review-actions.js
@@ -2,11 +2,19 @@ import { ReviewApi } from '../../services/rest-api';
 import { actionTypes } from '../constants';
 import { receiveApiErrorResponse } from './error-actions';
 
-const { FETCH_REVIEW_BEGIN, FETCH_REVIEW_SUCCESS } = actionTypes;
+const {
+  FETCH_REVIEW_BEGIN,
+  REVIEW_EFFECT_BEGIN,
+  FETCH_REVIEW_SUCCESS,
+} = actionTypes;
 const api = new ReviewApi();
 
 const fetchReviewBegin = () => ({
   type: FETCH_REVIEW_BEGIN,
+});
+
+const startReviewEffect = () => ({
+  type: REVIEW_EFFECT_BEGIN,
 });
 
 const fetchReviewSuccess = (reviewData) => ({
@@ -14,20 +22,84 @@ const fetchReviewSuccess = (reviewData) => ({
   payload: reviewData,
 });
 
-const fetchReviewByRequestId = (reviewRequestId, userId) => async (
+export const fetchReviewByRequestId = (reviewRequestId, userId) => async (
   dispatch
 ) => {
-  dispatch(fetchReviewBegin());
   try {
+    dispatch(fetchReviewBegin());
     const result = await api.getReviewByRequest(reviewRequestId);
     const userReview = result.find(({ author }) => author === userId);
     if (userReview) {
-      dispatch(fetchReviewSuccess(result));
+      return dispatch(fetchReviewSuccess(userReview));
     }
-    dispatch(fetchReviewSuccess({}));
+    return dispatch(fetchReviewSuccess({}));
   } catch (error) {
-    dispatch(receiveApiErrorResponse(error));
+    return dispatch(receiveApiErrorResponse(error.message));
   }
 };
 
-export default fetchReviewByRequestId;
+export const createReview = (requestId, authorId, gradeItems) => async (
+  dispatch
+) => {
+  const data = {
+    author: authorId,
+    grade: [...gradeItems],
+    requestId,
+  };
+  const body = {
+    githubId: authorId,
+    data,
+  };
+  try {
+    dispatch(startReviewEffect());
+    const result = await api.createReview(body);
+    if (result.id) {
+      return dispatch(fetchReviewSuccess(result));
+    }
+    return dispatch(receiveApiErrorResponse(result.message));
+  } catch (error) {
+    return dispatch(receiveApiErrorResponse(error.message));
+  }
+};
+
+export const editReview = (authorId, reviewId, gradeItems) => async (
+  dispatch
+) => {
+  const body = {
+    githubId: authorId,
+    reviewId,
+    data: {
+      grade: [...gradeItems],
+    },
+  };
+  try {
+    dispatch(startReviewEffect());
+    const result = await api.editReview(body);
+    if (result.error) {
+      return dispatch(receiveApiErrorResponse(result.message));
+    }
+    return dispatch(fetchReviewSuccess(result));
+  } catch (error) {
+    return dispatch(receiveApiErrorResponse(error.message));
+  }
+};
+
+export const toggleReviewStatus = (userId, reviewId, requiredState) => async (
+  dispatch
+) => {
+  const body = {
+    githubId: userId,
+    reviewId,
+    requiredState,
+  };
+  try {
+    dispatch(startReviewEffect());
+    const result = await api.toggleReviewState(body);
+    if (result.error) {
+      return dispatch(receiveApiErrorResponse(result.message));
+    }
+    return dispatch(fetchReviewSuccess(result));
+  } catch (error) {
+    return dispatch(receiveApiErrorResponse(error.message));
+  }
+};
--- src/redux/reducers/review-reducer.js
+++ src/redux/reducers/review-reducer.js
@@ -6,9 +6,14 @@ const initialState = {
 };
 
 const reviewReducer = (state = initialState, { type, payload }) => {
-  const { FETCH_REVIEW_BEGIN, FETCH_REVIEW_SUCCESS } = actionTypes;
+  const {
+    FETCH_REVIEW_BEGIN,
+    REVIEW_EFFECT_BEGIN,
+    FETCH_REVIEW_SUCCESS,
+  } = actionTypes;
   switch (type) {
     case FETCH_REVIEW_BEGIN:
+    case REVIEW_EFFECT_BEGIN:
       return { ...state, isLoading: true };
     case FETCH_REVIEW_SUCCESS:
       return { ...state, review: { ...payload }, isLoading: false };
